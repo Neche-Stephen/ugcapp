46248b06cee00914dcc349a314721bb5b3051069
Chinecherem <chinecheremubawike@gmail.com>
2024-01-31 18:17:50 +0100

    New changes

--- src/Routes/signup/SignUp.tsx
+++ src/Routes/signup/SignUp.tsx
@@ -6,7 +6,29 @@ import styles from './SignUp.module.css';
 export default function SignUp() {
   return (
     <div className={`${styles.signup}`}>
-      
+      <div className={`${styles.welcome}`}>Welcome</div>
+      <div className={`${styles.signup_start}`}>Sign Up to get Started</div>
+      <form>
+        <div className={`flex justify-center`}>
+            <input className={`${styles.signup_input}`} type="text" placeholder='First name'/>
+        </div>
+        <div className={`flex justify-center`}>
+         <input className={`${styles.signup_input}`} type="text" placeholder='Last name'/>
+        </div>
+        <div className={`flex justify-center`}>
+         <input className={`${styles.signup_input}`} type="email" placeholder='Email Address'/>
+        </div>
+        <div className={`flex justify-center`}>
+         <input className={`${styles.signup_input}`} type="number" placeholder='Phone Number'/>
+        </div>
+        <div className={`flex justify-center`}>
+         <input className={`${styles.signup_input}`} type="password" placeholder='Password'/>
+        </div>
+        <div className={`flex justify-center`}>
+         <input className={`${styles.signup_input}`} type="password" placeholder='Repeat Password'/>
+        </div>
+        
+      </form>
     </div>
   )
 }
