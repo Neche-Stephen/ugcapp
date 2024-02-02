09abada3418ebe794b3b9b5c64f61478b0da58fb
Chinecherem <chinecheremubawike@gmail.com>
2024-02-02 15:20:11 +0100

    New changes

--- src/Routes/signup/SignUp.tsx
+++ src/Routes/signup/SignUp.tsx
@@ -1,6 +1,8 @@
-
+import { FaArrowRight } from "react-icons/fa";
 
 import styles from './SignUp.module.css';
+
+import images from "../../assets";
 
 
 export default function SignUp() {
@@ -8,27 +10,50 @@ export default function SignUp() {
     <div className={`${styles.signup}`}>
       <div className={`${styles.welcome}`}>Welcome</div>
       <div className={`${styles.signup_start}`}>Sign Up to get Started</div>
-      <form>
-        <div className={`flex justify-center`}>
-            <input className={`${styles.signup_input}`} type="text" placeholder='First name'/>
+      <div className={`${styles.signup_form_wrapper}`}>
+        <form className={`${styles.signup_form}`}>
+            <div className={`flex justify-center`}>
+                <input className={`${styles.signup_input}`} type="text" placeholder='First name'/>
+            </div>
+            <div className={`flex justify-center`}>
+            <input className={`${styles.signup_input}`} type="text" placeholder='Last name'/>
+            </div>
+            <div className={`flex justify-center`}>
+            <input className={`${styles.signup_input}`} type="email" placeholder='Email Address'/>
+            </div>
+            <div className={`flex justify-center`}>
+            <input className={`${styles.signup_input}`} type="number" placeholder='Phone Number'/>
+            </div>
+            <div className={`flex justify-center`}>
+            <input className={`${styles.signup_input}`} type="password" placeholder='Password'/>
+            </div>
+            <div className={`flex justify-center`}>
+            <input className={`${styles.signup_input}`} type="password" placeholder='Repeat Password'/>
+            </div>
+            
+          </form>
+      </div>
+      <div className={`flex justify-center items-center gap-5`}>
+          <div className={`${styles.signup_text}`}>Sign Up</div>
+          <div><FaArrowRight color="white"/></div>
+      </div>
+
+      <div className={`flex justify-center gap-5 mt-10`}>
+        <div className={`${styles.signup_social_container}`}>
+          <img src={images.Ellipse} alt="" />
+          <img className={`${styles.signup_social}`} src={images.google} alt="" />
         </div>
-        <div className={`flex justify-center`}>
-         <input className={`${styles.signup_input}`} type="text" placeholder='Last name'/>
-        </div>
-        <div className={`flex justify-center`}>
-         <input className={`${styles.signup_input}`} type="email" placeholder='Email Address'/>
-        </div>
-        <div className={`flex justify-center`}>
-         <input className={`${styles.signup_input}`} type="number" placeholder='Phone Number'/>
-        </div>
-        <div className={`flex justify-center`}>
-         <input className={`${styles.signup_input}`} type="password" placeholder='Password'/>
-        </div>
-        <div className={`flex justify-center`}>
-         <input className={`${styles.signup_input}`} type="password" placeholder='Repeat Password'/>
+
+        <div className={`${styles.signup_social_container}`}>
+          <img src={images.Ellipse} alt="" />
+          <img className={`${styles.signup_social}`} src={images.apple} alt="" />
         </div>
         
-      </form>
+      </div>
+      {/* login here */}
+      <div className={`${styles.login} flex justify-center mt-10`}>
+          Log in, <b>here</b>
+      </div>
     </div>
   )
 }
--- src/assets/images/image.tsx
+++ src/assets/images/image.tsx
@@ -1,3 +1,7 @@
 import hand_laptop from './hand_laptop.png';
+import google from './google.png';
+import apple from './apple.png';
+import Ellipse from './Ellipse.png';
 
-export default { hand_laptop}
+
+export default { hand_laptop, google, apple, Ellipse}
